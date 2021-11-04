2e84ca958b714aefc8e31772a4775f81bfb61253
0xff0a0000 <damien.gitter@telindus.lu>
2021-11-04 16:45:12 +0100

    Update creatdb.sql

--- TP/Scripts/creatdb.sql
+++ TP/Scripts/creatdb.sql
@@ -1,4 +1,84 @@
-use webgoat_coins;
+/* Create the full set of Classic Models Tables */
+
+CREATE TABLE Customers (
+  customerNumber INTEGER NOT NULL,
+  customerName VARCHAR(50) NOT NULL,
+  logoFileName VARCHAR(100) NULL,
+  contactLastName VARCHAR(50) NOT NULL,
+  contactFirstName VARCHAR(50) NOT NULL,
+  phone VARCHAR(50) NOT NULL,
+  addressLine1 VARCHAR(50) NOT NULL,
+  addressLine2 VARCHAR(50) NULL,
+  city VARCHAR(50) NOT NULL,
+  state VARCHAR(50) NULL,
+  postalCode VARCHAR(15) NULL,
+  country VARCHAR(50) NOT NULL,
+  salesRepEmployeeNumber INTEGER NULL,
+  creditLimit DOUBLE NULL,
+  PRIMARY KEY (customerNumber)
+);
+
+CREATE TABLE CustomerLogin (
+        email VARCHAR(100) NOT NULL,
+        customerNumber INTEGER NOT NULL,
+        password VARCHAR(40) NOT NULL,
+        question_id SMALLINT NULL,
+        answer VARCHAR(50) NULL,
+        PRIMARY KEY (email)
+);
+
+CREATE TABLE SecurityQuestions (
+        question_id SMALLINT NOT NULL,
+        question_text VARCHAR(400) NOT NULL,
+        PRIMARY KEY (question_id)
+);
+
+
+CREATE TABLE Employees (
+  employeeNumber INTEGER NOT NULL,
+  lastName VARCHAR(50) NOT NULL,
+  firstName VARCHAR(50) NOT NULL,
+  extension VARCHAR(10) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  officeCode VARCHAR(10) NOT NULL,
+  reportsTo INTEGER NULL,
+  jobTitle VARCHAR(50) NOT NULL,
+  PRIMARY KEY (employeeNumber)
+);
+
+CREATE TABLE Offices (
+  officeCode VARCHAR(10) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  phone VARCHAR(50) NOT NULL,
+  addressLine1 VARCHAR(50) NOT NULL,
+  addressLine2 VARCHAR(50) NULL,
+  state VARCHAR(50) NULL,
+  country VARCHAR(50) NOT NULL,
+  postalCode VARCHAR(15) NOT NULL,
+  territory VARCHAR(10) NOT NULL,
+  PRIMARY KEY (officeCode)
+);
+
+CREATE TABLE OrderDetails (
+  orderNumber INTEGER NOT NULL,
+  productCode VARCHAR(15) NOT NULL,
+  quantityOrdered INTEGER NOT NULL,
+  priceEach DOUBLE NOT NULL,
+  orderLineNumber SMALLINT NOT NULL,
+  PRIMARY KEY (orderNumber, productCode)
+);
+
+CREATE TABLE Orders (
+  orderNumber INTEGER NOT NULL,
+  orderDate DATETIME NOT NULL,
+  requiredDate DATETIME NOT NULL,
+  shippedDate DATETIME NULL,
+  status VARCHAR(15) NOT NULL,
+  comments TEXT NULL,
+  customerNumber INTEGER NOT NULL,
+  PRIMARY KEY (orderNumber)
+);
+
 
 CREATE TABLE Payments (
   customerNumber INTEGER NOT NULL,  
@@ -40,4 +120,3 @@ CREATE TABLE Comments(
         comment TEXT NOT NULL,
         PRIMARY KEY (commentNumber)
 );
-
