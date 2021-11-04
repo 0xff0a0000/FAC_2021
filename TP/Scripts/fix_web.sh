af2fbec9505baa2d21003d95b3ebcaf1e4574d84
0xff0a0000 <damien.gitter@telindus.lu>
2021-11-04 16:33:10 +0100

    Update fix_web.sh

--- TP/Scripts/fix_web.sh
+++ TP/Scripts/fix_web.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo mysql < fix.sql
-sudo mysql <  creatdb.sql
+
 
 rm /var/www/html/WebGoat/DB_Scripts/load_webgoatcoins.sql
 cp load_webgoatcoins.sql /var/www/html/WebGoat/DB_Scripts/load_webgoatcoins.sql
