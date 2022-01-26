4deba964a9620f2840fee3bf9e7cacdc7a5bb4ba
0xff0a0000 <damien.gitter@telindus.lu>
2022-01-26 09:52:01 +0100

    Update install_wifi1.sh

--- Wifi/install_wifi1.sh
+++ Wifi/install_wifi1.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 apt update
-apt install -y build-essential bc linux-headers-5.15.0-kali2-amd64 git
+apt install -y build-essential bc linux-headers-5.15.0-kali2-amd64 linux-image-5.15.0-kali2-amd64 git
+sudo update-grub
 echo " si aucune erreur -> reboot"
