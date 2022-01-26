096a49ea6b09826da968ff06a9705e6e8d5b8844
0xff0a0000 <damien.gitter@telindus.lu>
2022-01-26 09:03:16 +0100

    Update install_wifi.sh

--- Wifi/install_wifi.sh
+++ Wifi/install_wifi.sh
@@ -2,9 +2,10 @@
 apt update
 #apt install -y build-essential bc linux-headers-5.2.0-kali2-amd64 git
 apt install -y build-essential bc linux-headers-5.15.0-kali2-amd64 git
-cd ~/Documents/
+cd /home/student/Documents/
 git clone https://github.com/aircrack-ng/rtl8188eus.git
 cd rtl8188eus
+cp /home/student/Documents/FAC_2021/Wifi/rtw_br_ext.c core/rtw_br_ext.c
 echo -e "blacklist r8188eu.ko\nblacklist r8188eu" > "/etc/modprobe.d/realtek.conf"
 make all
 make install
