9b60acef1de5bfda7c360deeb6daad77857c0ad4
0xff0a0000 <damien.gitter@telindus.lu>
2022-01-26 01:13:59 +0100

    Update install_wifi.sh

--- Wifi/install_wifi.sh
+++ Wifi/install_wifi.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt update
-apt install -y build-essential bc linux-headers-5.2.0-kali2-amd64 git
+#apt install -y build-essential bc linux-headers-5.2.0-kali2-amd64 git
+apt install -y build-essential bc linux-headers-5.14.0-kali4-amd64 git
 cd /root/Documents/
 git clone https://github.com/aircrack-ng/rtl8188eus.git
 cd rtl8188eus
